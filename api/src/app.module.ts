5f7b1879d1d90def3f5f5b239a29950c4068ffba
Yusk <psixopat1998@gmail.com>
2025-05-02 20:59:05 +0300

    fix(app.module): update imports, services and controllers

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AdminsService } from './admins/admins.service';
 import { AdminsModule } from './admins/admins.module';
-import { AuthService } from './auth/auth.service';
-import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AdminsModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, AdminsService, AuthService],
+  imports: [AuthModule, AdminsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
